a5e2e474cab14afaf5dc48f66d4f4a494248e0f3
Jhosef Cardich Palma <jacp.tech.91@gmail.com>
2020-09-16 19:34:35 +0200

    chore : 'this' lesson

--- repaso/this.js
+++ repaso/this.js
@@ -33,3 +33,11 @@ const videos2 = {
 };
 
 videos2.showTags();
+/**
+ * For fns inside Objects: 'this' refers to the object calling the function.
+For fns not called by objects - Eg call back functions -    'this' refers to the global object.
+For fns defined using arrow functions - 'this' simple refers to the object that is executing the 
+arrow function - Irrespective of object's scope. Meaning:  if the arrow function is called from a 
+global scope? 'this' will refer to the global object. If the arrow function is called with in a function 
+which belongs to an object?'this' will refer to the calling function's scope and in this case the object itself.
+ */
