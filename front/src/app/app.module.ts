c4f1fccc04568fd95c090041624b7942ef229c78
Jhosef Cardich Palma <jacp.tech.91@gmail.com>
2020-09-17 03:46:48 +0200

    fix: view composition fixed

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './components/task/task.component';
 import { TaskService } from './services/task.service';
 
 @NgModule({
   declarations: [AppComponent, TaskComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [TaskService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- front/src/app/components/task/task.component.ts
+++ front/src/app/components/task/task.component.ts
@@ -1,5 +1,9 @@
+import { importType } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
-
+// Services
+import { TaskService } from '../../services/task.service';
+//Models
+import { Task } from '../../models/task.model';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
@@ -8,5 +12,16 @@ import { Component, OnInit } from '@angular/core';
 export class TaskComponent implements OnInit {
 
 
+  constructor(private TaskService: TaskService) { }
+  tasks$: Task[] = [];
 
+  ngOnInit() {
+    console.log(this.TaskService.getTasksFake());
+    return this.TaskService.getTasksFake();
+
+    /*return this.TaskService.getApiTasks().subscribe((data: any) => {
+      this.tasks$ = data;
+      console.log(this.tasks$);
+    });*/
+  }
 }
